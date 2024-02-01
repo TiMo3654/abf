0a2d7672ddc11e48a3c25fb20ba1c0a97c38c8ea
Till <moldenhauer.till@gmail.com>
2024-02-01 17:11:08 +0100

    update movement function return type

--- util.py
+++ util.py
@@ -929,7 +929,9 @@ def rotate(A: dict) -> tuple:
     return new_width, new_height
 
 
-def reenter(A : dict, layout_zone : dict) -> tuple:                      # Only activated and working in case of a totally lost participant
+def reenter(A : dict, layout_zone : dict) -> list:                      # Only activated and working in case of a totally lost participant
+
+    new_A                               = copy.deepcopy(A)
 
     participant_left_of_layout_zone     = (A['xmin'] < layout_zone['xmin'])
 
@@ -950,12 +952,16 @@ def reenter(A : dict, layout_zone : dict) -> tuple:                      # Only 
 
     y_min_new                           = layout_zone['ymin'] if participant_below_layout_zone else y_min_new
 
-    y_min_new                           = (layout_zone['ymin'] + layout_zone['height'] - A['height']) if participant_above_layout_zone else y_min_new     
+    y_min_new                           = (layout_zone['ymin'] + layout_zone['height'] - A['height']) if participant_above_layout_zone else y_min_new
+
+    new_A['xmin'], new_A['ymin']        = x_min_new, y_min_new
     
-    return x_min_new, y_min_new
+    return [new_A]
 
 
 def evade(A : dict, layout_zone : dict, layout_zone_edge : str, align_position : str) -> list:
+
+    new_A               = copy.deepcopy(A)
 
     if layout_zone_edge == 'north':
 
@@ -1027,44 +1033,65 @@ def evade(A : dict, layout_zone : dict, layout_zone_edge : str, align_position :
 
     else:
         
-        print('No correct edge given!')      
+        print('No correct edge given!')  
 
-    return x_min_new, y_min_new
+    new_A['xmin'], new_A['ymin']    = x_min_new, y_min_new    
+
+    return [new_A]
 
 
-def center(A: dict) -> tuple:
+def center(A: dict) -> list:
+
+    new_A                           = copy.deepcopy(A)
      
-    x_min_new   = int(A['freespace']['xmin'] + 0.5 * A['freespace']['width'] - 0.5 * A['width'])
-    y_min_new   = int(A['freespace']['ymin'] + 0.5 * A['freespace']['height'] - 0.5 * A['height'])
+    x_min_new                       = int(A['freespace']['xmin'] + 0.5 * A['freespace']['width'] - 0.5 * A['width'])
+    y_min_new                       = int(A['freespace']['ymin'] + 0.5 * A['freespace']['height'] - 0.5 * A['height'])
 
-    return x_min_new, y_min_new
+    new_A['xmin'], new_A['ymin']    = x_min_new, y_min_new
+
+    return [new_A]
 
 
-def linger(A: dict) -> tuple:
+def linger(A: dict) -> list:
+
+    new_A   = copy.deepcopy(A)
      
-    return A['xmin'], A['ymin']
+    return [new_A]
 
 
-def budge(A: dict, direction : str) -> tuple:   
+def budge(A: dict, direction : str) -> list:   
+
+    new_A                           = copy.deepcopy(A)
 
     x_min_new       = int(A[direction]['xmin'] + 0.5 * A[direction]['width'] - 0.5 * A['width'])
     y_min_new       = int(A[direction]['ymin'] + 0.5 * A[direction]['height'] - 0.5 * A['height'])
+
+    new_A['xmin'], new_A['ymin']    = x_min_new, y_min_new
      
-    return x_min_new, y_min_new
+    return [new_A]
 
 
-def swap(A: dict, B: dict) -> tuple:
+def swap(A: dict, B: dict) -> list:
+
+    new_A                           = copy.deepcopy(A)
+    new_B                           = copy.deepcopy(B)
      
-    x_min_new_A     = int(B['freespace']['xmin'] + 0.5 * B['freespace']['width'] - 0.5 * A['width'])
-    y_min_new_A     = int(B['freespace']['ymin'] + 0.5 * B['freespace']['height'] - 0.5 * A['height'])
+    x_min_new_A                     = int(B['freespace']['xmin'] + 0.5 * B['freespace']['width'] - 0.5 * A['width'])
+    y_min_new_A                     = int(B['freespace']['ymin'] + 0.5 * B['freespace']['height'] - 0.5 * A['height'])
 
-    x_min_new_B     = int(A['freespace']['xmin'] + 0.5 * A['freespace']['width'] - 0.5 * B['width'])
-    y_min_new_B     = int(A['freespace']['ymin'] + 0.5 * A['freespace']['height'] - 0.5 * B['height'])
+    x_min_new_B                     = int(A['freespace']['xmin'] + 0.5 * A['freespace']['width'] - 0.5 * B['width'])
+    y_min_new_B                     = int(A['freespace']['ymin'] + 0.5 * A['freespace']['height'] - 0.5 * B['height'])
 
-    return x_min_new_A, y_min_new_A, x_min_new_B, y_min_new_B
+    new_A['xmin'], new_A['ymin']    = x_min_new_A, y_min_new_A
+    new_B['xmin'], new_B['ymin']    = x_min_new_B, y_min_new_B
+
+    return [new_A, new_B]
 
 
-def pair(A : dict, B : dict, direction : str) -> tuple:
+def pair(A : dict, B : dict, direction : str) -> list:
+
+    new_A                           = copy.deepcopy(A)
+    new_B                           = copy.deepcopy(B)
 
     if direction == 'horizontal-push-right' :
     
@@ -1093,10 +1120,17 @@ def pair(A : dict, B : dict, direction : str) -> tuple:
         y_min_new_A     = int(B['ymin'] + B['height'] - 0.5 * A['height'])
         y_min_new_B     = int(B['ymin'] - 0.5 * A['height'])
 
+    new_A['xmin'], new_A['ymin']    = x_min_new_A, y_min_new_A
+    new_B['xmin'], new_B['ymin']    = x_min_new_B, y_min_new_B
 
-    return x_min_new_A, y_min_new_A, x_min_new_B, y_min_new_B
 
-def hustle(A : dict, B : dict) -> tuple:
+    return [new_A, new_B]
+
+
+def hustle(A : dict, B : dict) -> list:
+
+    new_A                           = copy.deepcopy(A)
+    new_B                           = copy.deepcopy(B)
     
     overlap, locations  = calculate_overlap(A, B)
 
@@ -1117,10 +1151,15 @@ def hustle(A : dict, B : dict) -> tuple:
 
     y_min_new_B         = B['ymin'] + delta_y
 
-    return A['xmin'], A['ymin'], x_min_new_B, y_min_new_B
+    new_A['xmin'], new_A['ymin']    = A['xmin'], A['ymin']
+    new_B['xmin'], new_B['ymin']    = x_min_new_B, y_min_new_B
+
+    return [new_A, new_B]
 
 
-def yielt(A : dict) -> tuple:      # Intentional typo in "yield" to avoid keyword
+def yielt(A : dict) -> list:      # Intentional typo in "yield" to avoid keyword
+
+    new_A             = copy.deepcopy(A)
      
     x_center_yield_poly     = A['yield-polygon']['xmin'] + 0.5 * A['yield-polygon']['width']
 
@@ -1129,8 +1168,10 @@ def yielt(A : dict) -> tuple:      # Intentional typo in "yield" to avoid keywor
     x_min_new_A             = x_center_yield_poly - 0.5 * A['width']
 
     y_min_new_A             = y_center_yield_poly - 0.5 * A['height']
+
+    new_A['xmin'], new_A['ymin']    = x_min_new_A, y_min_new_A
     
-    return x_min_new_A, y_min_new_A
+    return [new_A]
 
 
 ## Action exploration and evaluation
@@ -1157,31 +1198,42 @@ def explore_action(A : dict, participants : dict, layout_zone : dict, leeway_coe
     adjuvant_action                 = []
     valid_action                    = []
 
-    new_A                           = copy.deepcopy(A)
-        
-    new_A['xmin'], new_A['ymin']    = action(new_A)
+    new_origins                     = action(A)     # list with either one entry (only a moved A) or two entries (moved A and B)
 
-    moved_A_conditions              = calculate_conditions(new_A, participants, layout_zone, leeway_coeffcient, conciliation_quota, critical_amount)
+    for new_origin in new_origins:
+    
+        new_A                           = copy.deepcopy(A)
+        new_participants                = copy.deepcopy(participants)
 
-    new_A.update(moved_A_conditions)
+        new_A['xmin'], new_A['ymin']    = new_origin
 
-    new_A['last-move']              = action.__name__
-
-    if new_A['last-move'] == 'reenter' or new_A['last-move'] == 'yielt':    # classification irrelevant in this case
-        
-        valid_action.append(new_A)
-
-    else:
-
-        action_classification           = classify_action(new_A)
-
-        if action_classification == 'adjuvant':
+        if new_A['idx'] in new_participants:
             
-            adjuvant_action.append(new_A)  
-        
-        elif action_classification == 'valid':
+            new_participants.remove(new_A['idx'])   # make sure, that the currentyl evaluated participnat is not in the participant list
+
+        # Implement action correction here
+
+        moved_A_conditions              = calculate_conditions(new_A, new_participants, layout_zone, leeway_coeffcient, conciliation_quota, critical_amount)
+
+        new_A.update(moved_A_conditions)
+
+        new_A['last-move']              = action.__name__
+
+        if new_A['last-move'] == 'reenter' or new_A['last-move'] == 'yielt':    # classification irrelevant in this case
+            
+            valid_action.append(new_A)
+
+        else:
+
+            action_classification           = classify_action(new_A)
+
+            if action_classification == 'adjuvant':
                 
-            valid_action.append(new_A)     
+                adjuvant_action.append(new_A)  
+            
+            elif action_classification == 'valid':
+                    
+                valid_action.append(new_A)     
     
     return adjuvant_action, valid_action
 
