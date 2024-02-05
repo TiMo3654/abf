751d785fce4a81c957fd85fec0daa53b1c40e7ab
Till <moldenhauer.till@gmail.com>
2024-02-05 09:29:58 +0100

    add sqrt to scaling

--- interaction.py
+++ interaction.py
@@ -1,12 +1,13 @@
 # Interaction control
 import copy
+import math
 
 def scale_layout_zone_linear(layout_zone : dict, scaling_factor : float) -> dict:
 
     new_layout_zone             = copy.deepcopy(layout_zone)
 
-    new_layout_zone['width']    = layout_zone['width'] * scaling_factor
+    new_layout_zone['width']    = layout_zone['width'] * math.sqrt(scaling_factor)
 
-    new_layout_zone['height']   = layout_zone['height'] * scaling_factor
+    new_layout_zone['height']   = layout_zone['height'] * math.sqrt(scaling_factor)
 
     return new_layout_zone
