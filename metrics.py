ce7f6dfd1f34560a4c2fdeab9e3fb0bc898b9dff
Till <moldenhauer.till@gmail.com>
2024-02-06 11:03:11 +0100

    add dead space metric

---
+++ metrics.py
@@ -0,0 +1,17 @@
+from util import *
+
+
+def calculate_dead_space(participants : dict, layout_zone : dict) -> float:
+
+    total_layout_area               = calculate_layout_area(layout_zone)
+
+    widths                          = [sub_dict.get('width') for sub_dict in participants.values()]
+
+    heights                         = [sub_dict.get('height') for sub_dict in participants.values()]
+
+    summed_participants_area        = sum([a * b for a, b in zip(widths, heights)])
+
+    
+    dead_space                      = summed_participants_area/total_layout_area
+
+    return dead_space
