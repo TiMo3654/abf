690e8aa3a1dd1d995db18addd0e7a1ec421d3da6
Till <moldenhauer.till@gmail.com>
2024-02-08 09:57:26 +0100

    add rotation again

--- swarm.py
+++ swarm.py
@@ -42,15 +42,15 @@ def one_round_of_interaction(participants : dict, layout_zone : dict, metric : s
 
         A                           = new_participants.pop(idx)  
 
-        #A_rotated                   = rotate(A)
-        #A_rotated_conditions        = calculate_conditions(A_rotated, new_participants, layout_zone, leeway_coefficient, conciliation_quota, critical_amount)
-        #A_rotated.update(A_rotated_conditions)
+        A_rotated                   = rotate(A)
+        A_rotated_conditions        = calculate_conditions(A_rotated, new_participants, layout_zone, leeway_coefficient, conciliation_quota, critical_amount)
+        A_rotated.update(A_rotated_conditions)
 
         possible_new_positions      = action_exploration(A, new_participants, layout_zone, leeway_coefficient, conciliation_quota, critical_amount)
 
-        #possible_new_positions_rot  = action_exploration(A_rotated, new_participants, layout_zone, leeway_coefficient, conciliation_quota, critical_amount)
+        possible_new_positions_rot  = action_exploration(A_rotated, new_participants, layout_zone, leeway_coefficient, conciliation_quota, critical_amount)
 
-        new_position                = determine_best_move(possible_new_positions, participants, metric) #+ possible_new_positions_rot
+        new_position                = determine_best_move(possible_new_positions + possible_new_positions_rot, participants, metric) #+ possible_new_positions_rot
 
         for moved_participant in new_position:
             
