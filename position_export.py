c7b4bda0e76094281518bdb6184e1a74cff19975
Till <moldenhauer.till@gmail.com>
2024-02-12 12:21:25 +0100

    add hex color to export

--- position_export.py
+++ position_export.py
@@ -1,15 +1,18 @@
 import pandas as pd
+from matplotlib import colors
 
 
 def positions2csv(participants : dict, layout_zone : dict):
 
-    df          = pd.DataFrame.from_dict(participants, orient = 'index')
+    df              = pd.DataFrame.from_dict(participants, orient = 'index')
 
-    df_reduced  = df[['idx', 'xmin', 'ymin', 'width', 'height', 'color']]
+    df['color_hex'] = [colors.to_hex(x) for x in list(df['color'].values)]
 
-    df_zone     = pd.DataFrame.from_dict(layout_zone, orient = 'index').transpose()
+    df_reduced      = df[['idx', 'xmin', 'ymin', 'width', 'height', 'color_hex']]
 
-    df_final    = pd.concat([df_zone, df_reduced])
+    df_zone         = pd.DataFrame.from_dict(layout_zone, orient = 'index').transpose()
+
+    df_final        = pd.concat([df_zone, df_reduced])
 
     df_final.to_csv('module_positions.csv', index = False)
 
