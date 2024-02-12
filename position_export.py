fc766768a9fbb7aeeefda05e106bfbfa59a2de9a
Till <moldenhauer.till@gmail.com>
2024-02-12 16:06:31 +0100

    adjust export to tikz

--- position_export.py
+++ position_export.py
@@ -1,16 +1,18 @@
 import pandas as pd
 from matplotlib import colors
 
+# Plot in tikz: https://gist.github.com/AugustUnderground/fc914edbc007c37ad3008d50701a7a7e
 
 def positions2csv(participants : dict, layout_zone : dict):
 
+
     df              = pd.DataFrame.from_dict(participants, orient = 'index')
 
-    df['color_hex'] = [colors.to_hex(x) for x in list(df['color'].values)]
+    df['color_hex'] = [colors.to_hex(x)[1:] for x in list(df['color'].values)]
 
     df_reduced      = df[['idx', 'xmin', 'ymin', 'width', 'height', 'color_hex']]
 
-    df_zone         = pd.DataFrame.from_dict(layout_zone, orient = 'index').transpose()
+    df_zone         = pd.DataFrame.from_dict(layout_zone | {'idx' : "~", 'color_hex' : 'ffffff'}, orient = 'index').transpose()
 
     df_final        = pd.concat([df_zone, df_reduced])
 
