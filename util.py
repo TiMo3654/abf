b8c9432b481f5412a57b620e25d96b0edba590d2
Till <moldenhauer.till@gmail.com>
2024-02-20 10:18:53 +0100

    even more streamlining

--- util.py
+++ util.py
@@ -2,26 +2,18 @@ import math
 
 def calculate_overlap(A : dict, B : dict) -> tuple:
 
-    x_A_min             = A['xmin']
-    y_A_min             = A['ymin']
-    x_A_max             = A['xmin'] + A['width']
-    y_A_max             = A['ymin'] + A['height']
+    x_A_min, y_A_min, x_A_max, y_A_max  = A['xmin'], A['ymin'], A['xmin'] + A['width'], A['ymin'] + A['height']
 
-    x_B_min             = B['xmin']
-    y_B_min             = B['ymin']
-    x_B_max             = B['xmin'] + B['width']
-    y_B_max             = B['ymin'] + B['height']
+    x_B_min, y_B_min, x_B_max, y_B_max  = B['xmin'], B['ymin'], B['xmin'] + B['width'], B['ymin'] + B['height']
 
     # Determine overlap direction (multiple can be true)
 
     if  (x_A_min >= x_B_max or x_A_max <= x_B_min) or (y_A_min >= y_B_max or y_A_max <= y_B_min):    # No horizontal or vertical overlap
 
-        overlapped          = False
-        locations           = [False, False, False, False, False, False]
+        overlap                 = {}
+        locations               = [False, False, False, False, False, False]
 
     else:
-
-        overlapped        = True
 
         A_fully_encloses_B      = (x_A_min <= x_B_min <= x_A_max) and (x_A_min <= x_B_max <= x_A_max) and  (y_A_min <= y_B_min <= y_A_max) and (y_A_min <= y_B_max <= y_A_max)
 
@@ -35,13 +27,10 @@ def calculate_overlap(A : dict, B : dict) -> tuple:
 
         south_edge_overlap      = (y_A_min > y_B_min) and (y_A_min < y_B_max)
         
-        locations   = [A_fully_encloses_B, B_fully_encloses_A, west_edge_overlap, east_edge_overlap, north_edge_overlap, south_edge_overlap]
+        locations               = [A_fully_encloses_B, B_fully_encloses_A, west_edge_overlap, east_edge_overlap, north_edge_overlap, south_edge_overlap]
 
-# Determine overlap coordinates
+        # Determine overlap coordinates
 
-    if not overlapped:
-        overlap = {}
-    else:
         if A_fully_encloses_B:
             x_Overlap_min, x_Overlap_max, y_Overlap_min, y_Overlap_max = x_B_min, x_B_max, y_B_min, y_B_max
         elif B_fully_encloses_A:
