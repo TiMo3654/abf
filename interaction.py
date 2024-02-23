6804d733502475a8e4e147994816e3361b0b8c2f
TiMo <moldenhauer.till@gmail.com>
2024-02-23 17:02:07 +0100

    update interaction

--- interaction.py
+++ interaction.py
@@ -2,29 +2,18 @@
 import copy
 import math
 
-def scale_layout_zone(layout_zone : dict, scaling_factor : float) -> dict:
+from collections import namedtuple
 
-    new_layout_zone             = copy.deepcopy(layout_zone)
+def scale_layout_zone(layout_zone : namedtuple, scaling_factor : float) -> dict:
 
-    new_layout_zone['width']    = layout_zone['width'] * scaling_factor
-
-    new_layout_zone['height']   = layout_zone['height'] * scaling_factor
+    new_layout_zone = layout_zone._replace(width = layout_zone.width * scaling_factor, height = layout_zone.height * scaling_factor)
 
     return new_layout_zone
 
 
 
-def reset_after_tightening(participants : dict) -> dict:
+def reset_after_tightening(participants : set) -> set:
 
-    reset_participants                          = copy.deepcopy(participants)
-        
-    id_list                                     = [p['idx'] for p in (reset_participants.values())]
-
-
-    for idx in id_list:
-
-        reset_participants[idx]['aversions']    = {}
-        reset_participants[idx]['clashes']      = {}
-        
+    reset_participants = {p._replace(aversions = {}, clashes = {}) for p in participants}        
 
     return reset_participants
